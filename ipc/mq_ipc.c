d145ef25e0eb38e1de9076ba1c64a5535deadd2b
Gavriel Karasin <gkarasin@gmail.com>
2020-02-18 21:58:20 +0200

    Fix vfork return value variable type

--- ipc/mq_ipc.c
+++ ipc/mq_ipc.c
@@ -19,7 +19,7 @@ void Reader(mqd_t mq);
 int main()
 {    
     mqd_t mq;
-    pthread_t wpid, rpid[NUM_OF_READERS];
+    pid_t wpid, rpid[NUM_OF_READERS];
     struct mq_attr attr = {0};
 
     /* initialize the queue attributes */
